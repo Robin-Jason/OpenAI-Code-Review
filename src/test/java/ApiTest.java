7b57f59e57b081c2dc7708af2ba25a8d948acdb9
Robin <1258956039@qq.com>
2026-02-01 20:59:56 +0800

    feat:test

---
+++ src/test/java/ApiTest.java
@@ -0,0 +1,14 @@
+
+
+import org.example.types.utils.BearerTokenUtils;
+
+public class ApiTest {
+
+    public static void main(String[] args) {
+
+        String apiKeySecret = "febc2789803a43b48ae4d02f7b8342d8.CTTTXZRtWv9QLoLo";
+
+        String token = BearerTokenUtils.getToken(apiKeySecret);
+        System.out.println(token);
+    }
+}
